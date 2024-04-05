a7df4c5e849f2110db6f6ca521c46394aec61b62
htejera <ohtejera@vivaldi.net>
2024-04-05 10:53:36 -0300

    Remove *.js from gitignore.

---
+++ main.js
@@ -0,0 +1,42 @@
+class GoatCounterIntegrationPlugin {
+    constructor(API, name, config) {
+        this.API = API;
+        this.name = name;
+        this.config = config;
+    }
+
+    addInsertions() {
+        this.API.addInsertion('publiiHead', this.addHeadCode, 1, this);
+    }
+
+    addHeadCode(rendererInstance) {
+        let scriptToLoad = '';
+
+        if (!rendererInstance.previewMode || this.config.previewMode) {
+            const goatCounterCode = this.config.goatCounterCode;
+            const visitCounter = this.config.showVisitCounter ? `
+                <script>
+                    var t = setInterval(function() {
+                        if (window.goatcounter && window.goatcounter.visit_count) {
+                            clearInterval(t)
+                            window.goatcounter.visit_count({
+                                append: '${this.config.visitCounterSelector}',
+                                type: '${this.config.visitCounterType}',
+                                no_branding: ${this.config.noBranding}
+                            })
+                        }
+                    }, 100)
+                </script>` : '';
+
+            scriptToLoad = `
+                ${visitCounter}
+                <script data-goatcounter="https://${goatCounterCode}.goatcounter.com/count"
+                        async src="//gc.zgo.at/count.js"></script>
+            `;
+        }
+
+        return scriptToLoad;
+    }
+}
+
+module.exports = GoatCounterIntegrationPlugin;
